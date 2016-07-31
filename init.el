7ba0cdaf41bbdf639723aebc7c8ef48a8f17576b
lmz <1206202883@qq.com>
2016-07-31 12:38:19 +0800

    add custom config

--- init.el
+++ init.el
@@ -1,3 +1,22 @@
+(defun dotspacemacs/layers ()
+  (setq-default
+    dotspacemacs-configuration-layers
+    '(
+      ivy
+      (auto-completion :variables auto-completion-enable-sort-by-usage t
+                      :disabled-for org markdown)
+      better-defaults
+      emacs-lisp
+      git
+      markdown
+      org
+      spell-checking
+      syntax-checking
+     )
+  )
+)
+
+
 (defun dotspacemacs/init ()
   (setq-default
    dotspacemacs-scratch-mode 'text-mode
@@ -10,7 +29,19 @@
                                :powerline-scale 1.1)
    dotspacemacs-line-numbers t
    dotspacemacs-maximized-at-startup t
-   dotspacemacs-additional-packages '(racket-mode))
+   dotspacemacs-additional-packages '(racket-mode)
+   dotspacemacs-editing-style 'vim
+   dotspacemacs-auto-save-file-location 'cache
+   dotspacemacs-highlight-delimiters 'all
+   dotspacemacs-startup-lists '((recents . 5)
+                                (projects . 7))
+   dotspacemacs-leader-key "SPC"
+   dotspacemacs-emacs-leader-key "M-m"
+   dotspacemacs-major-mode-leader-key ","
+   dotspacemacs-major-mode-emacs-leader-key "C-M-m"
+   dotspacemacs-emacs-command-key ":"
+
+   )
 )
 
 
